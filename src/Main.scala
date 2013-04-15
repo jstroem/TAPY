399a3937468448c938fddc323205754b54e7811e
Jesper Lindstrøm Nielsen <me@jstroem.com>
2013-04-15 10:13:41 +0200

    added multiple files run-setup

--- src/Main.scala
+++ src/Main.scala
@@ -7,16 +7,56 @@ import _root_.org.python.antlr.runtime.tree._;
 import tapy.cfg._
 
 object Main {
-  var node: mod = null
-  var cfg: ControlFlowGraph = null
-  
-  def main(args: Array[String]): Unit = {
-    
-    val parser: BaseParser = new BaseParser(new ANTLRFileStream("tests/greet.py"), "tests/greet.py", "ascii");
-    node = parser.parseModule()
-    
-    cfg = node.accept(CFGGeneratorVisitor);
-    
-    println(node.toStringTree());
-  }
+	var node: mod = null
+	var cfg: ControlFlowGraph = null
+
+	def main(args: Array[String]): Unit = {
+
+		args.foreach((f) => {
+			var file = new File(f)
+			if (file.isFile()){
+				analyzeFile(file, opts)
+			} else if (file.isDirectory()) {	
+				analyzeDirectory(file, opts)
+			} else {
+				System.err.println("Couldn't open: " + f)
+			}
+		})
+
+		val parser: BaseParser = new BaseParser(new ANTLRFileStream("tests/greet.py"), "tests/greet.py", "ascii");
+		node = parser.parseModule()
+
+		cfg = node.accept(CFGGeneratorVisitor);
+
+		println(node.toStringTree());
+	}
+
+	def analyze(file: File) : Unit = {
+		(dir,fname,fext) = splitFilename(file)
+
+		val parser: BaseParser = new BaseParser(new ANTLRFileStream(file.getPath()), file.getPath(), "ascii");
+		node = parser.parseModule()
+		cfg = node.accept(CFGGeneratorVisitor);
+		println(node.toStringTree());
+	}
+
+	def analyzeDirectory(dir : File) : Unit = {
+		dir.listFiles().foreach((file) => {
+			if (file.isFile()){
+				analyze(file, opts)
+			} else if (file.isDirectory()) {	
+				analyzeDirectory(file, opts)
+			}
+		})
+	}
+
+	def splitFilename(f : File) : (String,String,String) = {
+		var dir = f.getParent() + "/"
+		var file = f.getName()
+		var idx = file.lastIndexOf('.')
+		var extension = if (idx >= 0)  file.substring(idx) else ""
+		var filename = if (idx >= 0) file.substring(0, idx) else file
+		(dir,filename,extension)
+	}
+
 }
