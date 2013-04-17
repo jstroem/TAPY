699cb0529552aed3261f36bcccdee58f4eddd555
Troels Leth Jensen <troelslethjensen@gmail.com>
2013-04-17 09:57:50 +0200

    Tabs ~> Spaces

--- src/Constants.scala
+++ src/Constants.scala
@@ -1,11 +1,11 @@
 package tapy.constants
 
 class UnOp extends Enumeration {
-	//	not, ~ 
-	val NOT, TILDE  = Value
+  //	not, ~
+  val NOT, TILDE  = Value
 }
 
 class BinOp extends Enumeration {
-	//  +     -      <=   >=   <   >   ==  !=   in  is, *     /    %    //    >>   <<   &    ^    |   **
-	val PLUS, MINUS, LTE, GTE, LT, GT, EQ, NEQ, IN, IS, MULT, DIV, MOD, IDIV, SHR, SHL, AND, XOR, OR, POW = Value
+  //  +     -      <=   >=   <   >   ==  !=   in  is, *     /    %    //    >>   <<   &    ^    |   **
+  val PLUS, MINUS, LTE, GTE, LT, GT, EQ, NEQ, IN, IS, MULT, DIV, MOD, IDIV, SHR, SHL, AND, XOR, OR, POW = Value
 }
