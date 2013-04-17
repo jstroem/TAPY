b4162f9b22f0abed8ff8ae2f6fbd97de43ae2404
Troels Leth Jensen <troelslethjensen@gmail.com>
2013-04-17 09:53:00 +0200

    Tabs ~> Spaces

--- src/Main.scala
+++ src/Main.scala
@@ -11,57 +11,57 @@ import java.io._
 import tapy.export._
 
 object Main {
-	var cfg: ControlFlowGraph = null
+  var cfg: ControlFlowGraph = null
 
-	def main(args: Array[String]): Unit = {
-		args.foreach((f) => {
-			var file = new File(f)
-			if (file.isFile()){
-				analyzeFile(file)
-			} else if (file.isDirectory()) {	
-				analyzeDirectory(file)
-			} else {
-				System.err.println("Couldn't open: " + f)
-			}
-		})
-	}
+  def main(args: Array[String]): Unit = {
+    args.foreach((f) => {
+      var file = new File(f)
+      if (file.isFile()){
+        analyzeFile(file)
+      } else if (file.isDirectory()) {
+        analyzeDirectory(file)
+      } else {
+        System.err.println("Couldn't open: " + f)
+      }
+    })
+  }
 
-	def analyzeFile(file: File) : Unit = {
-		val parser: BaseParser = new BaseParser(new ANTLRFileStream(file.getPath()), file.getPath(), "ascii")
-		var (dir,fname,fext) = splitFilename(file)
+  def analyzeFile(file: File) : Unit = {
+    val parser: BaseParser = new BaseParser(new ANTLRFileStream(file.getPath()), file.getPath(), "ascii")
+    var (dir,fname,fext) = splitFilename(file)
 
-		val ast = parser.parseModule()
-		println("\n----------\n")
-		println("Pretty printing AST of \"" + file + "\"\n")
-		println("\n" + ast.accept(ASTPrettyPrinter))
-		
+    val ast = parser.parseModule()
+    println("\n----------\n")
+    println("Pretty printing AST of \"" + file + "\"\n")
+    println("\n" + ast.accept(ASTPrettyPrinter))
+
     println("\n----------\n")
     println("Generating CFG of \"" + file + "\"\n")
-		cfg = ast.accept(CFGGeneratorVisitor)
+    cfg = ast.accept(CFGGeneratorVisitor)
 
-		/*
-		val graphvizGraph = cfg.generateGraphvizGraph()
+    /*
+     val graphvizGraph = cfg.generateGraphvizGraph()
+                 
+     GraphvizExporter.export(graphvizGraph, new PrintStream(dir + fname+".cfg.dot"))
+     Runtime.getRuntime().exec("dot -Tgif -o "+dir + fname+".cfg.gif " + dir + fname+".cfg.dot")*/
+  }
 
-		GraphvizExporter.export(graphvizGraph, new PrintStream(dir + fname+".cfg.dot"))
-		Runtime.getRuntime().exec("dot -Tgif -o "+dir + fname+".cfg.gif " + dir + fname+".cfg.dot")*/
-	}
+  def analyzeDirectory(dir : File) : Unit = {
+    dir.listFiles().foreach((file) => {
+      if (file.isFile()){
+        analyzeFile(file)
+      } else if (file.isDirectory()) {
+        analyzeDirectory(file)
+      }
+    })
+  }
 
-	def analyzeDirectory(dir : File) : Unit = {
-		dir.listFiles().foreach((file) => {
-			if (file.isFile()){
-				analyzeFile(file)
-			} else if (file.isDirectory()) {	
-				analyzeDirectory(file)
-			}
-		})
-	}
-
-	def splitFilename(f : File) : (String,String,String) = {
-		var dir = f.getParent() + "/"
-		var file = f.getName()
-		var idx = file.lastIndexOf('.')
-		var extension = if (idx >= 0)  file.substring(idx) else ""
-		var filename = if (idx >= 0) file.substring(0, idx) else file
-		(dir,filename,extension)
-	}
+  def splitFilename(f : File) : (String,String,String) = {
+    var dir = f.getParent() + "/"
+    var file = f.getName()
+    var idx = file.lastIndexOf('.')
+    var extension = if (idx >= 0)  file.substring(idx) else ""
+    var filename = if (idx >= 0) file.substring(0, idx) else file
+    (dir,filename,extension)
+  }
 }
