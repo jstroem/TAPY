234b584633aaa457e3fcbeabe5ce3ea8dd4007e0
Christoffer Q. Adamsen <christofferqa@gmail.com>
2013-04-18 10:06:51 +0200

    Removed a file

--- tests/greet.py
+++
@@ -1,8 +0,0 @@
-def greet(firstname = '<no firstname>', lastname = '<no lastname>'):
-    print 'Hello', firstname, lastname
-greet('John', 'Doe')
-greet('John', lastname='Jack')
-x = ['John', 'Doe']
-greet(*x)
-y = {'lastname': 'Doe'}
-greet('John', **y)
