59d2d439f25738e72410cbae062dd0bd6cc37f67
Jesper Lindstrøm Nielsen <me@jstroem.com>
2013-04-24 15:55:03 +0200

    added visitList

--- src/cfg/CFGGeneratorVisitor.scala
+++ src/cfg/CFGGeneratorVisitor.scala
@@ -702,16 +702,16 @@ object CFGGeneratorVisitor extends VisitorBase[ControlFlowGraph] {
       val addFuncReg = nextRegister()
       val readGetFuncNode = ReadPropertyNode(resultReg, "add", addFuncReg)
 
-      val newSetAndGetFuncCfg = newSetCfg.addNode(readGetFuncNode).connectNodes(newSetCfg.exitNodes, readGetFuncNode).setExitNode(readGetFuncNode)
-      return pair.foldLeft(newSetAndGetFuncCfg)((accCfg,a) => {
+      val newSetAndAddFuncCfg = newSetCfg.addNode(readGetFuncNode).connectNodes(newSetCfg.exitNodes, readGetFuncNode).setExitNode(readGetFuncNode)
+      return pair.foldLeft(newSetAndAddFuncCfg)((accCfg,a) => {
         val (cfg,reg) = a
-        val callGetNode = CallNode(nextRegister(), addFuncReg, List(reg))
+        val callAddNode = CallNode(nextRegister(), addFuncReg, List(reg))
         accCfg.combineGraphs(cfg)
               .connectNodes(accCfg.exitNodes, cfg.entryNodes)
-              .addNode(callGetNode)
-              .connectNodes(cfg.exitNodes, callGetNode)
+              .addNode(callAddNode)
+              .connectNodes(cfg.exitNodes, callAddNode)
               .setEntryNodes(accCfg.entryNodes)
-              .setExitNode(callGetNode)
+              .setExitNode(callAddNode)
       })
     } else return newSetCfg
   }
@@ -845,7 +845,33 @@ object CFGGeneratorVisitor extends VisitorBase[ControlFlowGraph] {
 
   override def visitList(node: ast.List): ControlFlowGraph = {
     println("visitList");
-    return null
+    val resultReg = nextRegister()
+    val newListCfg = ControlFlowGraph.makeSingleton(NewListNode(resultReg))
+
+    val pair = node.getInternalElts().toList.map((a) => {
+      val cfg = a.accept(this)
+      val reg = lastExpressionRegister
+      (cfg,reg)
+    })
+
+    lastExpressionRegister = resultReg
+
+    if (pair.length > 0) {
+      val appendFuncReg = nextRegister()
+      val readAppendFuncNode = ReadPropertyNode(resultReg, "append", appendFuncReg)
+
+      val newListAndAppendFuncCfg = newListCfg.addNode(readAppendFuncNode).connectNodes(newListCfg.exitNodes, readAppendFuncNode).setExitNode(readAppendFuncNode)
+      return pair.foldLeft(newListAndAppendFuncCfg)((accCfg,a) => {
+        val (cfg,reg) = a
+        val callAppendNode = CallNode(nextRegister(), appendFuncReg, List(reg))
+        accCfg.combineGraphs(cfg)
+              .connectNodes(accCfg.exitNodes, cfg.entryNodes)
+              .addNode(callAppendNode)
+              .connectNodes(cfg.exitNodes, callAppendNode)
+              .setEntryNodes(accCfg.entryNodes)
+              .setExitNode(callAppendNode)
+      })
+    } else return newListCfg
   }
 
   override def visitTuple(node: Tuple): ControlFlowGraph = {
