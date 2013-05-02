6bba93b055dc304ec6561da16e848a9b585e1cd7
Jesper Lindstrøm Nielsen <me@jstroem.com>
2013-05-02 09:56:42 +0200

    added Boolean Lattice

--- src/lattices/BooleanLattice.scala
+++ src/lattices/BooleanLattice.scala
@@ -1,75 +1,63 @@
-package tapy.sign
+package tapy.lattices
 
 import tapy.mfw._
 
-abstract class Sign
-case class QuestionMark() extends Sign
-case class Plus() extends Sign
-case class Zero() extends Sign
-case class Minus() extends Sign
-case class Bottom() extends Sign
-//case class One extends Sign
-//case class PlusZero extends Sign
-//case class MinusZero extends Sign
+object BooleanLattice {
+  sealed trait Boolean
+  case class True() extends Boolean
+  case class False() extends Boolean
+  case class Bottom() extends Boolean
+  case class Bool() extends Boolean
+} 
 
-
-class SignLattice extends Lattice[Sign] {
-  def top: Sign = QuestionMark()
-  def bottom: Sign = Bottom()
+class BooleanLattice extends Lattice[BooleanLattice.Boolean] {
+  def top: BooleanLattice.Boolean = BooleanLattice.Bool()
+  def bottom: BooleanLattice.Boolean = BooleanLattice.Bottom()
   
   // a >= b
-  def compare(a: Sign, b: Sign) = (a, b) match {
-    case (QuestionMark(), _)  => true
-
-    case (Plus(), Plus())     => true
-    case (Plus(), Bottom())   => true
-    case (Plus(), _)          => false
-
-    case (Minus(), Minus())   => true
-    case (Minus(), Bottom())  => true
-    case (Minus(), _)         => false
-
-    case (Zero(), Zero())     => true
-    case (Zero(), Bottom())   => true
-    case (Zero(), _)          => false
-
-    case (Bottom(), Bottom()) => true
-    case (Bottom(), _)        => false
+  def compare(a: BooleanLattice.Boolean, b: BooleanLattice.Boolean) = (a, b) match {
+    case (BooleanLattice.Bool(), _)  => true
+    case (_, BooleanLattice.Bottom()) => true
+    case (BooleanLattice.True(), BooleanLattice.True()) => true
+    case (BooleanLattice.False(), BooleanLattice.False()) => true
+    case _ => false
   }
 
-  def leastUpperBound(a: Sign, b: Sign) = (a, b) match {
-    case (QuestionMark(), _) => QuestionMark()
+  def leastUpperBound(a: BooleanLattice.Boolean, b: BooleanLattice.Boolean) = (a, b) match {
+    case (BooleanLattice.Bool(), _) =>                        BooleanLattice.Bool()
+    case (_, BooleanLattice.Bool()) =>                        BooleanLattice.Bool()
 
-    case (Plus(), Plus())    => Plus()
-    case (Plus(), Bottom())  => Plus()
-    case (Plus(), _)         => QuestionMark()
+    case (BooleanLattice.True(), BooleanLattice.True()) =>    BooleanLattice.True()
+    case (BooleanLattice.False(), BooleanLattice.False()) =>  BooleanLattice.False()
 
-    case (Minus(), Minus())  => Minus()
-    case (Minus(), Bottom()) => Minus()
-    case (Minus(), _)        => QuestionMark()
 
-    case (Zero(), Zero())    => Zero()
-    case (Zero(), Bottom())  => Zero()
-    case (Zero(), _)         => QuestionMark()
+    case (BooleanLattice.False(), BooleanLattice.Bottom()) => BooleanLattice.False()
+    case (BooleanLattice.True(), BooleanLattice.Bottom()) =>  BooleanLattice.True()
 
-    case (Bottom(), _)       => b
+    case (BooleanLattice.Bottom(), BooleanLattice.False()) => BooleanLattice.False()
+    case (BooleanLattice.Bottom(), BooleanLattice.True()) =>  BooleanLattice.True()
+
+    case (BooleanLattice.Bottom(), BooleanLattice.Bottom()) =>  BooleanLattice.Bottom()
+
+    case (_, _) =>  BooleanLattice.Bool()
   }
 
-  def greatestLowerBound(a: Sign, b: Sign) = (a, b) match {
-    case (QuestionMark(), _)       => b
+  def greatestLowerBound(a: BooleanLattice.Boolean, b: BooleanLattice.Boolean) = (a, b) match {
+    case (BooleanLattice.Bottom(), _) =>                        BooleanLattice.Bottom()
+    case (_, BooleanLattice.Bottom()) =>                        BooleanLattice.Bottom()
 
-    case (Plus(), Plus())          => Plus()
-    case (Plus(), QuestionMark())  => Plus()
-    case (Plus(), _)               => Bottom()
+    case (BooleanLattice.True(), BooleanLattice.True()) =>    BooleanLattice.True()
+    case (BooleanLattice.False(), BooleanLattice.False()) =>  BooleanLattice.False()
 
-    case (Minus(), Minus())        => Minus()
-    case (Minus(), QuestionMark()) => Minus()
-    case (Minus(), _)              => Bottom()
 
-    case (Zero(), Zero())          => Zero()
-    case (Zero(), QuestionMark())  => Zero()
-    case (Zero(), _)               => Bottom()
+    case (BooleanLattice.False(), BooleanLattice.Bool()) => BooleanLattice.False()
+    case (BooleanLattice.True(), BooleanLattice.Bool()) =>  BooleanLattice.True()
 
-    case (Bottom(), _)             => Bottom()
+    case (BooleanLattice.Bool(), BooleanLattice.False()) => BooleanLattice.False()
+    case (BooleanLattice.Bool(), BooleanLattice.True()) =>  BooleanLattice.True()
+
+    case (BooleanLattice.Bool(), BooleanLattice.Bool()) =>  BooleanLattice.Bool()
+
+    case (_, _) =>  BooleanLattice.Bottom()
   }
 }
