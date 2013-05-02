05df8d68b0fa9485f7519c355f4bb8de0fabff17
Christoffer Q. Adamsen <christofferqa@gmail.com>
2013-05-02 09:52:15 +0200

    undefined and none lattices

--- src/lattices/UndefinedLattice.scala
+++ src/lattices/UndefinedLattice.scala
@@ -1,75 +1,30 @@
-package tapy.sign
+package tapy.lattices
 
 import tapy.mfw._
 
-abstract class Sign
-case class QuestionMark() extends Sign
-case class Plus() extends Sign
-case class Zero() extends Sign
-case class Minus() extends Sign
-case class Bottom() extends Sign
-//case class One extends Sign
-//case class PlusZero extends Sign
-//case class MinusZero extends Sign
+object UndefinedLattice {
+  sealed trait UndefinedElt
 
+  case class Bottom() extends UndefinedElt
+  case class Undefined() extends UndefinedElt
+}
 
-class SignLattice extends Lattice[Sign] {
-  def top: Sign = QuestionMark()
-  def bottom: Sign = Bottom()
+class UndefinedLattice extends Lattice[UndefinedLattice.UndefinedElt] {
+  def top: UndefinedLattice.UndefinedElt = UndefinedLattice.Undefined()
+  def bottom: UndefinedLattice.UndefinedElt = UndefinedLattice.Bottom()
   
   // a >= b
-  def compare(a: Sign, b: Sign) = (a, b) match {
-    case (QuestionMark(), _)  => true
-
-    case (Plus(), Plus())     => true
-    case (Plus(), Bottom())   => true
-    case (Plus(), _)          => false
-
-    case (Minus(), Minus())   => true
-    case (Minus(), Bottom())  => true
-    case (Minus(), _)         => false
-
-    case (Zero(), Zero())     => true
-    case (Zero(), Bottom())   => true
-    case (Zero(), _)          => false
-
-    case (Bottom(), Bottom()) => true
-    case (Bottom(), _)        => false
+  def compare(a: UndefinedLattice.UndefinedElt, b: UndefinedLattice.UndefinedElt): Boolean = return (a, b) match {
+    case (UndefinedLattice.Undefined(), _) => true
+    case (UndefinedLattice.Bottom(), UndefinedLattice.Bottom()) => true
+    case (UndefinedLattice.Bottom(), UndefinedLattice.Undefined()) => false
   }
-
-  def leastUpperBound(a: Sign, b: Sign) = (a, b) match {
-    case (QuestionMark(), _) => QuestionMark()
-
-    case (Plus(), Plus())    => Plus()
-    case (Plus(), Bottom())  => Plus()
-    case (Plus(), _)         => QuestionMark()
-
-    case (Minus(), Minus())  => Minus()
-    case (Minus(), Bottom()) => Minus()
-    case (Minus(), _)        => QuestionMark()
-
-    case (Zero(), Zero())    => Zero()
-    case (Zero(), Bottom())  => Zero()
-    case (Zero(), _)         => QuestionMark()
-
-    case (Bottom(), _)       => b
+  
+  def leastUpperBound(a: UndefinedLattice.UndefinedElt, b: UndefinedLattice.UndefinedElt): UndefinedLattice.UndefinedElt = {
+    return if (a == NoneLattice.None() || b == NoneLattice.None()) UndefinedLattice.Undefined() else UndefinedLattice.Bottom()
   }
-
-  def greatestLowerBound(a: Sign, b: Sign) = (a, b) match {
-    case (QuestionMark(), _)       => b
-
-    case (Plus(), Plus())          => Plus()
-    case (Plus(), QuestionMark())  => Plus()
-    case (Plus(), _)               => Bottom()
-
-    case (Minus(), Minus())        => Minus()
-    case (Minus(), QuestionMark()) => Minus()
-    case (Minus(), _)              => Bottom()
-
-    case (Zero(), Zero())          => Zero()
-    case (Zero(), QuestionMark())  => Zero()
-    case (Zero(), _)               => Bottom()
-
-    case (Bottom(), _)             => Bottom()
+  
+  def greatestLowerBound(a: UndefinedLattice.UndefinedElt, b: UndefinedLattice.UndefinedElt): UndefinedLattice.UndefinedElt = {
+    return if (a == NoneLattice.Bottom() || b == NoneLattice.Bottom()) UndefinedLattice.Bottom() else UndefinedLattice.Undefined()
   }
 }
